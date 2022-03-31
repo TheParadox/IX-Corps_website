c5dd603409811ddac66d869c02d9b8e2810ab510
TheParadox <TheParadox@users.noreply.github.com>
2022-03-31 15:51:48 -0500

    Example coding comments
    - which I need to start doing anyways...

--- app/Http/Controllers/Unit/IndividualController.php
+++ app/Http/Controllers/Unit/IndividualController.php
@@ -24,10 +24,25 @@ class IndividualController extends Controller
     public function index(Request $request, $memberID)
     {
         //$data = DB::table('users')->where('id', $member)->first();
+        
+        /*
+        The function handles verifying that all models used are valid. 
+        If there are no valid models (i.e., the refered object does not exist), it will set a default value.
+        If there is any values we wish to output, we try to set as much of it here.
+        That way there is only the absolutly required control and flow statements within the view.
+        
+        Examples of a required control statement in the view are:
+        If the user doesn't have a rank - then maybe we don't want to display the label as there is nothing there
+        Or lists, we need to use foreach loops to go through the array as we have no idea how many items might in it.
+        -- Of course we could 'set-it' with pagenation, but don't hard code it.
+        */
+        
+        
         $data = User::findOrFail($memberID);
 
         $perm = Permission::where('level', auth()->user()->permissions)->first();
 
+        //Both isDischarged and isLOA are booleans, convert the meaning to 'english'
         if($data->isDischarged){
             $data->status = 'Discharged';
         } else if($data->isLOA) {
@@ -36,7 +51,7 @@ class IndividualController extends Controller
             $data->status = 'Active';
         }
 
-
+        //Someplace where we still want to show the label as well as output a specific value.
         $regiment = Regiment::find($data->regiment_id);
         if($regiment === null) {
             $data->regiment_name = "Not Assigned a Regiment";
@@ -57,13 +72,16 @@ class IndividualController extends Controller
         } else {
             $data->rank = $rank->grade;
         }
-        //use DateTime - can use date_diff... if needed...
-
+        
+        //use DateTime - can use date_diff... if needed... For using regular PHP...
+        //This is the current method used to determine the 'time since' - i.e., how long has the person been with the group.
         $today = Carbon::now()->format('Y-m-d');
         $joined = Carbon::createFromFormat('Y-m-d', $data->dateJoined);
         //$data->tis = date_diff($joined, $today);
         $data->tis = Carbon::parse($data->dateJoined)->longAbsoluteDiffForHumans();
 
+        //There is going to be alot of awards... There is probably a more efficient/effective way to do it.
+        //Potential candidate for optimization.
         $awardsList = Award::all()->toArray();
 
         $earnedAwards = json_decode($data->awards, true);
